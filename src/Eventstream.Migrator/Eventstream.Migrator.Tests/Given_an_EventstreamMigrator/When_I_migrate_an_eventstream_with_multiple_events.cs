cdfe63063df3c414ad75bcad3cf8acfc736f1a26
Gitte Vermeiren <gitte.vermeiren@gmail.com>
2014-09-06 21:17:55 +0200

    added tests

--- src/Eventstream.Migrator/Eventstream.Migrator.Tests/Given_an_EventstreamMigrator/When_I_migrate_an_eventstream_with_multiple_events.cs
+++ src/Eventstream.Migrator/Eventstream.Migrator.Tests/Given_an_EventstreamMigrator/When_I_migrate_an_eventstream_with_multiple_events.cs
@@ -1,4 +1,5 @@
-﻿using Eventstream.Migrator.Tests.Utilities;
+﻿using System.Collections.Generic;
+using Eventstream.Migrator.Tests.Utilities;
 using Moq;
 using Xunit;
 
@@ -10,6 +11,9 @@ namespace Eventstream.Migrator.Tests.Given_an_EventstreamMigrator
         private Mock<IGetEventstreamMigrations> _migrationgetter;
         private Mock<IWriteAnEventstream> _eventstreamwriter;
         private EventStreamMigrator _sut;
+        private Mock<IMigrate> _mockMigration;
+        private SomeEventBase _someEvent;
+        private SomeEventBase _migratedEvent;
 
         public override void Arrange()
         {
@@ -22,7 +26,21 @@ namespace Eventstream.Migrator.Tests.Given_an_EventstreamMigrator
                     return readcounter <= 4;
 
                 });
+
+            _mockMigration = new Mock<IMigrate>();
+            var migratedEvents = new List<SomeEventBase>
+            {
+                _migratedEvent
+            };
+            _mockMigration.Setup(migration => migration.Migrate(_someEvent))
+                .Returns(migratedEvents);
+            var migrates = new List<IMigrate>
+            {
+                _mockMigration.Object
+            };
             _migrationgetter = new Mock<IGetEventstreamMigrations>();
+            _migrationgetter.Setup(getter => getter.GetMigrations())
+                .Returns(migrates);
             _eventstreamwriter = new Mock<IWriteAnEventstream>();
 
             _sut = new EventStreamMigrator(_eventstreamreader.Object, _migrationgetter.Object, _eventstreamwriter.Object);
@@ -43,6 +61,24 @@ namespace Eventstream.Migrator.Tests.Given_an_EventstreamMigrator
         public void It_tells_the_eventstreamreader_to_get_the_next_event_while_there_are_events()
         {
             _eventstreamreader.Verify(reader => reader.Get<SomeEventBase>(), Times.Exactly(4));
+        }
+
+        [Fact]
+        public void It_asks_the_migrationgetter_for_the_migration()
+        {
+            _migrationgetter.Verify(getter => getter.GetMigrations(), Times.Once);
+        }
+
+        [Fact]
+        public void It_applies_each_migration_to_the_eventstream()
+        {
+            _mockMigration.Verify(migration => migration.Migrate(_someEvent), Times.Exactly(4));
+        }
+
+        [Fact]
+        public void It_saves_4_events_to_the_eventstreamwriter()
+        {
+            _eventstreamwriter.Verify(writer => writer.Save(_migratedEvent), Times.Exactly(4));
         }
     }
 }
